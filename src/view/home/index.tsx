4a3f5beb8856d135fc19e39599c8783a783f724e
lucas paiva <lucas.paiva@mundiale.com.br>
2023-01-09 12:19:38 -0300

    fix: create header

--- src/view/home/index.tsx
+++ src/view/home/index.tsx
@@ -1,11 +1,21 @@
-import { Box } from "@chakra-ui/react";
+import { Flex, Link, Stack, Text } from "@chakra-ui/react";
 import { memo } from "react";
 
 const HomeView = () => {
     return (
-        <Box bg='tomato' w='100%' p={4} color='white'>
-            This is the Box
-        </Box>
+        <Flex bg='#000' p={4} color='white' as="header" justifyContent='center' alignItems='center'>
+            <Flex width='100%' maxWidth='1216px' alignItems='center' justifyContent='space-between'>
+                <Text>Lucas Paiva</Text>
+            </Flex>
+
+            <Stack direction={["column", "row"]} spacing='24px'>
+                <Link fontSize='14px' fontWeight='bold'>Inicio</Link>
+                <Link fontSize='14px' fontWeight='bold'>Sobre</Link>
+                <Link fontSize='14px' fontWeight='bold'>Habilidades</Link>
+                <Link fontSize='14px' fontWeight='bold'>Projetos</Link>
+                <Link fontSize='14px' fontWeight='bold'>Contato</Link>
+            </Stack>
+        </Flex>
     );
 };
 export default memo(HomeView);
