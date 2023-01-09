23932bf7ba86a53b2c539bcb5980ee874ede0e91
lucas paiva <lucas.paiva@mundiale.com.br>
2023-01-09 14:56:22 -0300

    fix:creating the application header and styling it on mobile

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "../globals/global";
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <ChakraProvider>
-            <Component {...pageProps} />;
+        <ChakraProvider theme={theme}>
+            <Component {...pageProps} />
         </ChakraProvider>
     );
 }
